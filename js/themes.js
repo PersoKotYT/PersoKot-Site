e9ce6b99487c1b164efa317ca1a87922a5e1bc69
Николай <108145848+PersoKotYT@users.noreply.github.com>
2022-06-24 21:54:44 +0500

    Add files via upload

---
+++ js/themes.js
@@ -0,0 +1,24 @@
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function() { ChangeTheme();});
+
+function ChangeTheme() {
+	let lightTheme = "assets/css/light.css";
+	let darkTheme = "assets/css/dark.css";
+
+	var currTheme = link.getAttribute("href");
+	var theme = "";
+
+	if(currTheme == lightTheme) {
+		currTheme = darkTheme;
+		theme = "dark";
+	}
+	else 
+	{
+		currTheme = lightTheme;
+		theme = "light";
+	}
+	link.setAttribute("href", currTheme);
+
+}
